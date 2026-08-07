5cf33f13b6d5a40794d8538e69789e15e306cdfc
vellsneha <work.vellsneha@gmail.com>
2026-08-07 17:58:40 -0400

    vite config changes 2

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,7 @@
-import path from "node:path";
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// Project site: https://vellsneha.github.io/snehavel/
 export default defineConfig({
-  base: "/",
   plugins: [react()],
-  resolve: {
-    alias: {
-      "@social": path.resolve(__dirname, "social"),
-    },
-  },
-});
+  base: '/',
+})
